0f8b43cf1f11e7b7c7785e80e4f326a1f3c302c9
_tireDep <wing997@naver.com>
2025-06-09 22:47:48 +0900

    네이티브 게임플레이 태그

---
+++ Warrior/Source/Warrior/Private/WarrirorGameplayTags.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WarrirorGameplayTags.h"
+
+namespace WarriorGameplayTags
+{
+	// Input Tags
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look")
+}
---
+++ Warrior/Source/Warrior/Public/WarrirorGameplayTags.h
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace WarriorGameplayTags
+{
+	// Input Tags
+	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)		// 이동 입력
+	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)		// 보기 입력
+}
